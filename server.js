0ab09f9492544a7c32446796f0e991265a2f2dbf
AmaniALnabhani <amanialnabhani7@gmail.com>
2023-07-13 19:46:21 +0400

    server work

--- server.js
+++ server.js
@@ -1,50 +1,41 @@
-// Setup empty JS object to act as endpoint for all routes
+/* Empty JS object to act as endpoint for all routes */
 projectData = {};
-// Express to run server and routes
+
+/* Express to run server and routes */
 const express = require('express');
 
-// Start up an instance of app
+/* Start up an instance of app */
 const app = express();
+
 /* Dependencies */
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
 const bodyParser = require('body-parser')
-// Cors for cross origin allowance
+/* Middleware*/
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const cors = require('cors');
 app.use(cors());
-// Initialize the main project folder
+
+/* Initialize the main project folder*/
 app.use(express.static('website'));
 
 const port = 3000;
 
-// Route
-app.get('/projectData', (req, res)=>{
-  res.status(200).send(projectData);
-});
 
-// POST route
-app.post('/projectData', (req,res)=>
-{
+app.post('/projectData', (req, res) => {
   projectData = {
-   date : req.body.date,
-   temp : req.body.temp,
-   content : req.body.content
+    date: req.body.date,
+    temp: req.body.temp,
+    content: req.body.content
   };
-
-
-
-res.status(200).json({
-  success: true,
-  message: "Data saved successfully",
-  data: projectData
-});
+  console.log(projectData);
+  res.status(200).json({
+    success: true,
+    message: "Data saved successfully",
+    data: projectData
+  });
 });
 
 // Start the server
 app.listen(port, () => {
-console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
-
-
--- website/app.js
+++ website/app.js
@@ -1,127 +1,124 @@
 document.addEventListener('DOMContentLoaded', () => {
-    //take generate button id
-    const generateButton = document.querySelector('#generate');
-    generateButton.addEventListener('click', generateButtonHandel);});
-  
-  async function generateButtonHandel() {
-    // take zipcode and point value
-    const zipCode = document.getElementById('zipcode').value;
-    const contentpoint = document.getElementById('point').value;
-    // create url
-    const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=100c669e1f70478e3704350b7dd901b2`;
-  
-    //check if user enter zip code or point (validation)
-    if (zipCode.length === 0 || contentpoint.length === 0) {
-      alert("please fill empty field");
+  const findButton = document.querySelector('#find');
+  findButton.addEventListener('click', handleFindButton);
+});
+
+async function handleFindButton() {
+  const zipcode = document.getElementById('zip').value;
+  const contentpoint = document.getElementById('feeling').value;
+  const url = `http://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&appid=c96e9d8d3c7b7e4a1346577c0bddbdad`;
+
+  if (zipcode.length === 0 || contentpoint.length === 0) {
+    alert("Please fill in all empty inputs!");
+    return;
+  }
+
+  try {
+    const weatherData = await fetchWeatherData(url);
+
+    if (!weatherData) {
+      alert("Failed to fetch weather data. Please try again later.");
       return;
     }
-      // try and chatch
-    try {
-      const weather_Data = await fetchWeatherData(url);
-      if (!weather_Data) {
-        alert("failed to fetch weather data");
-        return;
-      }
-  
-      const { main, weather, name, sys, clouds } = weather_Data;
-  
-      if (!main || !weather || weather.length === 0 || !name || !sys || !sys.country) {
-        alert("Weather data is incomplete. Please try again later.");
-        return;
-      }
-  
-      const { temp } = main;
-      const { description } = weather[0];
-      const date = getCurrentDate();
-      const country = sys.country;
-      const cloudCoverage = clouds && clouds.all !== undefined ? clouds.all : 'N/A';
-      const data = {
-        date,
-        temp: convertToCelsius(temp),
-        description,
-        country,
-        name,
-        clouds: cloudCoverage,
-        content: contentpoint,
-      };
-  
-      await postData('http://localhost:3000/projectData', data);
-      updateUI();
-      
-      // Display the entry-holder section
-      const entryHolder = document.querySelector('.entry-holder');
-      entryHolder.style.display = 'block';
-    } catch (error) {
-      console.log(error);
-      alert("an error, Please try again");
+
+    const { main, weather, name, sys, clouds } = weatherData;
+
+    if (!main || !weather || weather.length === 0 || !name || !sys || !sys.country) {
+      alert("Weather data is incomplete. Please try again later.");
+      return;
     }
+
+    const { temp } = main;
+    const { description } = weather[0];
+    const date = getCurrentDate();
+    const country = sys.country;
+    const cloudCoverage = clouds && clouds.all !== undefined ? clouds.all : 'N/A';
+    const data = {
+      date,
+      temp: convertToCelsius(temp),
+      description,
+      country,
+      name,
+      clouds: cloudCoverage,
+      content: contentpoint,
+    };
+
+    await postData('http://localhost:3000/projectData', data);
+    updateUI();
+    
+    // Display the entry-holder section
+    const entryHolder = document.querySelector('.holder');
+    entryHolder.style.display = 'block';
+  } catch (error) {
+    console.log(error);
+    alert(" Please try again later.");
   }
-  
-  // featch wather data function
-  async function fetchWeatherData(url) {
-    try {
-      const Response = await fetch(url);
-      return await Response.json();
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }
-  }
- //get current date
-  function getCurrentDate() {
-    let d = new Date();
-    return `${d.getDate()}.${d.getMonth() + 1}.${d.getFullYear()}`;
-  }
-  /// update UI function
-  async function updateUI() {
-    const datetime = document.getElementById('datetime');
-    const temperature = document.getElementById('temperature');
-    const clouds = document.getElementById('clouds');
-    const content = document.getElementById('content');
-    const city = document.getElementById('city');
-    const country = document.getElementById('country');
-  
-    try {
-      const data = await getData('http://localhost:8000/projectData');
-  
-      datetime.innerText = data.date;
-      temperature.innerText = `${data.temp}°C`;
-      content.innerText = data.content;
-     
-    } catch (error) {
-      console.log(error);
-      alert("Error Please try again later.");
-    }
-  }
-  
-  function convertToCelsius(temp) {
-    return Math.round(temp - 273.15);
-  }
-  
-  async function postData(url, data) {
-    try {
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      });
-      return await response.json();
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }
-  }
-  
-  async function getData(url) {
-    try {
-      const response = await fetch(url);
-      return await response.json();
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }
-  }
+}
 
 
+async function fetchWeatherData(url) {
+  try {
+    const response = await fetch(url);
+    return await response.json();
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+function getCurrentDate() {
+  const currentDate = new Date();
+  return `${currentDate.getDate()}.${currentDate.getMonth() + 1}.${currentDate.getFullYear()}`;
+}
+
+async function updateUI() {
+  const date = document.getElementById('date');
+  const temp = document.getElementById('tempe');
+  const clouds = document.getElementById('clouds');
+  const content = document.getElementById('content');
+  const city = document.getElementById('city');
+  const country = document.getElementById('country');
+
+  try {
+    const data = await getData('http://localhost:3000/projectData');
+
+    date.innerText = data.date;
+    temp.innerText = `${data.temp}°C`;
+
+    content.innerText = data.content;
+  
+  } catch (error) {
+    console.log(error);
+    alert(" updating the UI. Please try again later.");
+  }
+}
+
+function convertToCelsius(temp) {
+  return Math.round(temp - 273.15);
+}
+
+async function postData(url, data) {
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    return await response.json();
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+async function getData(url) {
+  try {
+    const response = await fetch(url);
+    return await response.json();
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
